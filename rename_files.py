636b4d9d69d86da16a1a53c973bd4ce9c6123659
Rohan Dongre <dongrerohan421@gmail.com>
2014-12-24 02:06:22 -0600

    Add file list

--- rename_files.py
+++ rename_files.py
@@ -1,7 +1,9 @@
+import os # imports libraries related to operating system
 
 def rename_files() : # defining function
 	#(1) get file names from a folder
-
+	file_list = os.listdir("/media/rohan/3814F06A14F02C8E/Study/Udacity/Python/prank/prank")
+	print(file_list)
 	#(2) for each file, rename filename
 
 rename_files() # calling this function
