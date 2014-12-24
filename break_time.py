e4ec304781085d6c6ed3dfc56beb2332f45f0523
Rohan Dongre <dongrerohan421@gmail.com>
2014-12-24 01:07:40 -0600

    Add total breaks three

--- break_time.py
+++ break_time.py
@@ -1,6 +1,11 @@
 import time
 import webbrowser
 
-time.sleep(10)
-webbrowser.open("https://www.youtube.com/watch?v=8JJrz_VDbx0")
+total_breaks = 3
+break_count = 0
+
+while(break_count < total_break):
+	time.sleep(10)
+	webbrowser.open("https://www.youtube.com/watch?v=8JJrz_VDbx0")
+	break_count = break_count + 1
 
