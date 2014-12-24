d26d7fce2855fb8f95f957cd896472bbc491efa1
Rohan Dongre <dongrerohan421@gmail.com>
2014-12-24 01:11:51 -0600

    Add program started time

--- break_time.py
+++ break_time.py
@@ -4,6 +4,7 @@ import webbrowser
 total_breaks = 3
 break_count = 0
 
+print ("This program started on on " + time.ctime())
 while(break_count < total_break):
 	time.sleep(10)
 	webbrowser.open("https://www.youtube.com/watch?v=8JJrz_VDbx0")
