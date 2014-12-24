a9ffede658b85062bcb6a97129350f6a12fcb26a
Rohan Dongre <dongrerohan421@gmail.com>
2014-12-24 17:04:08 -0600

    change turtle's shape, color and speed

--- mindstorms.py
+++ mindstorms.py
@@ -5,6 +5,10 @@ def draw_square() : # define function
 	window.bgcolor("red") # add background color for window
 	
 	brad = turtle.Turtle()
+	brad.shape("turtle") # set shape of turtle
+	brad.color("yellow")  # set turtle color
+	brad.speed(2)	     # set turtle speed
+
 	brad.forward(100) # draw line forward in one direction
 	brad.right(90)    # turn right at 90 degree
 	brad.forward(100)
